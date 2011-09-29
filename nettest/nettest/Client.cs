93689a6d02a57667b78ce670d69e7ab51080acb8
unknown <Owen@.(none)>
2011-09-30 00:33:47 +0100

    first commit, cleaned up repo

---
+++ nettest/nettest/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Xna.Framework;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Threading;
+
+namespace nettest
+{
+    public class Client
+    {
+        Socket _socket;
+        int counter = 0;
+        bool isConnected = false;
+        Timer timer;
+        public int attempts = 0;
+        public void Connect (IAsyncResult result)
+        {
+            isConnected = true;
+        }
+        public Client(IPEndPoint endPoint)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            TryConnect();            
+        }
+        void Retry(object state)
+        {            
+            if (_socket.Connected == false)
+            {
+                ++attempts;
+                TryConnect();
+            }
+        }
+        public void TryConnect()
+        {
+            _socket.BeginConnect(new IPEndPoint(IPAddress.Parse("192.168.1.17"), 8024), new AsyncCallback(Connect), _socket);
+            if (timer == null)
+            {
+                timer = new Timer(new TimerCallback(Retry), timer, 1500, 0);
+            }
+            else
+            {
+                timer.Change(1500, 0);
+            }
+        }
+        public void Update(GameTime gameTime)
+        {
+            counter += gameTime.ElapsedGameTime.Milliseconds;
+            if (counter > 1000)
+            {               
+                if (_socket.Connected)
+                {
+                    DataChunk chunk = new DataChunk();
+                    chunk.strData = String.Format("{0} ms have elapsed, connected after {1} attempts", gameTime.TotalGameTime.Milliseconds, attempts);
+                    IFormatter formatter = new BinaryFormatter();
+                    byte[] data = System.Text.Encoding.ASCII.GetBytes(chunk.strData);                      
+                    _socket.Send(data);
+                }
+                counter = 0;
+            }
+        }
+        public void Disconnect()
+        {
+            _socket.Disconnect(true);
+        }
+    }
+}
---
+++ nettest/nettest/Server.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using Microsoft.Xna.Framework;
+
+namespace nettest
+{
+    public class GameServer
+    {
+        int portNumber;
+        Socket socket;
+        public bool isActive = false;
+        List<Socket> sockets = new List<Socket>();
+        public List<Connection> connections = new List<Connection>();
+        public void Accept(IAsyncResult result)
+        {
+            Socket s = (Socket)result.AsyncState;
+            connections.Add(new Connection(this, s.EndAccept(result)));
+            s.BeginAccept(new AsyncCallback(Accept), s);
+        }
+        public void End(Connection connectionToEnd)
+        {
+            connections.Remove(connectionToEnd);
+        }
+        public GameServer(int port = 8024)
+        {
+            portNumber = port;            
+        }
+        public void Listen()
+        {
+            isActive = true;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 8024));
+            socket.Listen(10);
+            socket.BeginAccept(new AsyncCallback(Accept), socket); 
+        }
+
+        public String LastText = "";
+
+    }
+}
